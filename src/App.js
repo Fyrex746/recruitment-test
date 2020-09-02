4ddc4ce5528fceece23562fea4d264a7872b35e1
Fyrex <louisquelliermsn@hotmail.fr>
2020-09-02 16:18:40 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from "react";
 import "./styles.css";
 
+const tasks = [
+  {
+    id: "step1",
+    text: "Learn development",
+  },
+  {
+    id: "step2",
+    text: "Create stuff",
+  },
+  {
+    id: "step3",
+    text: "????",
+  },
+  {
+    id: "step4",
+    text: "PROFIT!!!",
+  },
+];
+
 export default function App() {
   return (
     <div>
       <h1>Todo List</h1>
       <ul>
-        <li key="step1">
-          <span className="deleteIcon">x</span>
-          <input type="checkbox" id="step1" name="step1" />
-          <label for="step1">Learn development</label>
-        </li>
-        <li key="step2">
-          <span className="deleteIcon">x</span>
-          <input type="checkbox" id="step2" name="step2" />
-          <label for="step2">Create stuff</label>
-        </li>
-        <li key="step3">
-          <span className="deleteIcon">x</span>
-          <input type="checkbox" id="step3" name="step3" />
-          <label for="step3">????</label>
-        </li>
-        <li key="step4">
-          <span className="deleteIcon">x</span>
-          <input type="checkbox" id="step4" name="step4" />
-          <label for="step4">PROFIT!!!</label>
-        </li>
+        {tasks.map(task => <TodoItem task={task} />)}
       </ul>
     </div>
   );
+}
+
+function TodoItem({task}) {
+  const {id, text} = task;
+  return <li key={id}>
+    <span className="deleteIcon">x</span>
+    <input type="checkbox" id={id} name={id} />
+    <label for={id}>{text}</label>
+  </li>
 }
