55ed1ed41e62ead9816308e0d5f400623d06e6e7
Fyrex <louisquelliermsn@hotmail.fr>
2020-09-02 16:39:11 +0200

    cross out task when done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 
-const tasks = [
+const tasksDefault = [
   {
     id: "step1",
     text: "Learn development",
+    isDone: false
   },
   {
     id: "step2",
     text: "Create stuff",
+    isDone: false
   },
   {
     id: "step3",
     text: "????",
+    isDone: false
   },
   {
     id: "step4",
     text: "PROFIT!!!",
+    isDone: false
   },
 ];
 
 export default function App() {
+  const [tasks, setTasks] = useState(tasksDefault);
+
   return (
     <div>
       <h1>Todo List</h1>
       <ul>
-        {tasks.map(task => <TodoItem task={task} />)}
+        {tasks.map(task => <TodoItem key={task.id} task={task} onSetDone={isDone => handleSetDone(task.id, isDone)} />)}
       </ul>
     </div>
   );
+
+  function handleSetDone(id, isDone) {
+    const nextTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isDone
+        }
+      } else {
+        return task;
+      }
+    });
+    setTasks(nextTasks);
+  }
 }
 
-function TodoItem({task}) {
-  const {id, text} = task;
+function TodoItem({task, onSetDone}) {
+  const {id, text, isDone} = task;
   return <li key={id}>
     <span className="deleteIcon">x</span>
-    <input type="checkbox" id={id} name={id} />
-    <label for={id}>{text}</label>
+    <input
+      type="checkbox"
+      id={id}
+      name={id}
+      checked={isDone}
+      onClick={e => onSetDone(e.target.checked)}
+    />
+    <label for={id} className={isDone ? 'done' : ''}>{text}</label>
   </li>
 }
